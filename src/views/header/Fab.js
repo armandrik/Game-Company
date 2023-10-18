6acab08e831fac765e88686bd03fb632e7913433
armandrik <mr.drik0181@gmail.com>
2023-10-18 14:03:27 +0330

    add responsive menu

--- src/views/header/Fab.js
+++ src/views/header/Fab.js
@@ -9,7 +9,7 @@ export const Fab = () => {
 
     return (
         <section className='fab' onClick={backToTop}>
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#01204b" className="bi bi-chevron-up" viewBox="0 0 16 16">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fillRule="#01204b" className="bi bi-chevron-up" viewBox="0 0 16 16">
                 <path fill-rule="evenodd" d="M7.646 4.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1-.708.708L8 5.707l-5.646 5.647a.5.5 0 0 1-.708-.708l6-6z" />
             </svg>
         </section>
---
+++ src/views/header/ResponsiveMenu.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export const ResponsiveMenu = ({close}) => {
+
+    return (
+        <section className='responsive-menu'>
+            <button onClick={close}>
+                <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" fill="#ffffff" className="bi bi-x-circle" viewBox="0 0 16 16">
+                    <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+                    <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
+                </svg>
+            </button>
+            <a href='#home' onClick={close}>Home</a>
+            <a href='#games' onClick={close}>Games</a>
+            <a href='#career' onClick={close}>Careers</a>
+            <a href='#about' onClick={close}>About</a>
+            <a href='#contact' onClick={close}>Contact</a>
+        </section>
+    )
+}
